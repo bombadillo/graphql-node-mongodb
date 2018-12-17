0385f204beda5e7dd7fe50340a813444a24c9561
bombadillo <mumfordnsons@gmail.com>
2018-12-17 15:42:31 +0000

    Add initiate src

---
+++ src/common/user/user.model.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+  }
+});
+
+userSchema.set('toObject', { virtuals: true });
+
+userSchema.method('toGraph', function toGraph(this: any) {
+  return JSON.parse(JSON.stringify(this));
+});
+
+export default mongoose.model('User', userSchema);
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { ApolloServer } from 'apollo-server';
+import { makeExecutableSchema } from 'graphql-tools';
+import * as mongoose from 'mongoose';
+
+import { userResolvers, userTypeDefs } from './common/user/user.schema';
+
+const MONGODB_URI = 'mongodb://localhost/graphql-demo';
+
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true }
+);
+
+const rootTypeDefs = `
+  type Query
+  type Mutation
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`;
+
+const schema = makeExecutableSchema({
+  typeDefs: [rootTypeDefs, userTypeDefs],
+  resolvers: userResolvers
+});
+
+const server = new ApolloServer({
+  schema,
+  formatError(error) {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      console.log(error);
+    }
+
+    return error;
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
